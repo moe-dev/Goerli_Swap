4c7b7c0e36256a1b81722882d830b71fca941746
Mohammad Cheikh <cheikh98mohammad@gmail.com>
2023-02-15 23:34:16 -0500

    working commit, TODO frontend work

---
+++ swap_ui/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Swap from "./pages/swap";
+export default function App() {
+  return (
+    <BrowserRouter>
+    <div className="max-h-screen items-center">
+      <Routes>
+          <Route path="/" index element={<Swap />} />
+      </Routes>
+</div>
+    </BrowserRouter>
+  );
+}
